7fa2075e1d9f1c629c36adb5715ae8cd34a1c017
Theophas <afomassetheophas@gmail.com>
2024-03-06 11:28:22 +0100

    commit

--- constants/index.ts
+++ constants/index.ts
@@ -122,29 +122,29 @@ export const plans = [
 export const transformationTypes = {
   restore: {
     type: "restore",
-    title: "Restore Image",
-    subTitle: "Refine images by removing noise and imperfections",
+    title: "Restorer Image",
+    subTitle: "Affiner les images en supprimant le bruit et les imperfections",
     config: { restore: true },
     icon: "image.svg",
   },
   removeBackground: {
     type: "removeBackground",
-    title: "Background Remove",
-    subTitle: "Removes the background of the image using AI",
+    title: "Supprimer Arrière Plan",
+    subTitle: "Supprime l’arrière-plan de l’image en utilisant l’IA",
     config: { removeBackground: true },
     icon: "camera.svg",
   },
   fill: {
     type: "fill",
-    title: "Generative Fill",
-    subTitle: "Enhance an image's dimensions using AI outpainting",
+    title: "Generer Image",
+    subTitle: "Améliorer les dimensions d’une image en utilisant l’extension d’image par IA",
     config: { fillBackground: true },
     icon: "stars.svg",
   },
   remove: {
     type: "remove",
-    title: "Object Remove",
-    subTitle: "Identify and eliminate objects from images",
+    title: "Supprimer Objet",
+    subTitle: "Identifier et éliminer des objets des images",
     config: {
       remove: { prompt: "", removeShadow: true, multiple: true },
     },
@@ -152,8 +152,8 @@ export const transformationTypes = {
   },
   recolor: {
     type: "recolor",
-    title: "Object Recolor",
-    subTitle: "Identify and recolor objects from the image",
+    title: "Recolorier Objet",
+    subTitle: "Identifier et recolorer des objets de l’image",
     config: {
       recolor: { prompt: "", to: "", multiple: true },
     },
